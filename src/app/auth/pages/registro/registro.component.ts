edd6006305447253d2899070dd558be68debabf9
Fernando Herrera <fernando.herrera85@gmail.com>
2021-01-02 13:11:45 -0500

    Fin sección 18

--- src/app/auth/pages/registro/registro.component.ts
+++ src/app/auth/pages/registro/registro.component.ts
@@ -21,8 +21,22 @@ export class RegistroComponent implements OnInit {
     password2: ['', [ Validators.required ]  ],
   }, {
     validators: [ this.validatorService.camposIguales('password','password2') ]
-  })
+  });
 
+
+  get emailErrorMsg(): string {
+    
+    const errors = this.miFormulario.get('email')?.errors;
+    if ( errors?.required ) {
+      return 'Email es obligatorio';
+    } else if ( errors?.pattern ) {
+      return 'El valor ingresado no tiene formato de correo';
+    } else if ( errors?.emailTomado ) {
+      return 'El email ya fue tomado';
+    }
+
+    return '';
+  }
 
   constructor( private fb: FormBuilder,
                private validatorService: ValidatorService,
@@ -44,20 +58,7 @@ export class RegistroComponent implements OnInit {
             && this.miFormulario.get(campo)?.touched;
   }
 
-  emailRequired() {
-    return this.miFormulario.get('email')?.errors?.required
-            && this.miFormulario.get('email')?.touched;
-  }
 
-  emailFormato() {
-    return this.miFormulario.get('email')?.errors?.pattern
-            && this.miFormulario.get('email')?.touched;
-  }
-  
-  emailTomado() {
-    return this.miFormulario.get('email')?.errors?.emailTomado
-            && this.miFormulario.get('email')?.touched;
-  }
 
 
   submitFormulario() {
